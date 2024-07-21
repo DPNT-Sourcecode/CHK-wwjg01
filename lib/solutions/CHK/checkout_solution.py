1fef5077b96be721ba2a0f775553f697d7c49b14
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-07-21 15:51:29 +0000

    Sun Jul 21 15:51:29 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -17,7 +17,7 @@ Our price table and offers:
 '''
 
 # Implement the price table as a dict
-# key - item SKU / id, value - list where 0th index is price, following indexes are tuples representing bulk deal, if any
+# key - item SKU / id, value - list where 0th index is price, following index is tuple representing bulk deal, if any
 price_table = {
     "A": [50, (3, 130)],
     "B": [30, (2, 45)],
@@ -27,13 +27,20 @@ price_table = {
 def checkout(skus):
     final_price = 0
     basket = {}
+    # Build our basket from the str first
     for item in skus:
         if not price_table.get(item, False):
             return -1
         else:
             basket[item] = basket.get(item, 0) + 1
+    
+    # Iterate over our basket and determine final price:
+    for item in basket:
+        num_of_deals = 0
+        remainder = 0
+        
+        if len(price_table[item]) > 1:
+            return 
 
             
         
-
-
