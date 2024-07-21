aff16d1d95218148db05407006575d5dc18d695f
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-07-21 16:18:34 +0000

    Sun Jul 21 16:18:34 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -24,19 +24,45 @@ price_table = {
     "C": [20],
     "D": [15]
 }
+#main basket class - stores basket info and applies offers
+class Basket():
+    total = 0 #total price of our basket
+    invalid = False #whether this is an invalid basket
+    items = {} # "item" : amount
+    offers = []
+    def __init__(self, skus):
+        for item in skus:
+            if not price_table.get(item, False):
+                self.invalid = True
+            else:
+                self.items[item] = self.items.get(item, 0) + 1
+        
+# example bulk offer format - A.3.130
+class BulkOffer():
+    def __init__(self, offer):
+        item, amt, price = offer.split('.')
+        self.item = item
+        self.amt = amt
+        self.price = price
+        
+# type for '... get one free' offers. Supports getting more than one free.
+class GofOffer():
+    def __init__(self, offer):
+        item, amt, bonus = offer.split('.')
+        self.item = item
+        self.amt = amt
+        self.bonus = bonus
+
+
 def checkout(skus):
     final_price = 0
-    basket = {}
     # Build our basket from the str first
-    for item in skus:
-        if not price_table.get(item, False):
-            return -1
-        else:
-            basket[item] = basket.get(item, 0) + 1
-    
+    basket = Basket(skus)
+    if basket.invalid:
+        return -1
     # Iterate over our basket and determine final price:
-    for item in basket:
-        num_items = basket[item]
+    for item in basket.items:
+        num_items = basket.items[item]
         price = price_table[item][0]
         
         if len(price_table[item]) > 1:
--- test/solution_tests/test_checkout.py
+++ test/solution_tests/test_checkout.py
@@ -2,4 +2,4 @@ from lib.solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        assert checkout_solution.checkout("AAABBBD") == 220 #130(deal) + 45(deal) + 30 + 15
+        assert checkout_solution.checkout("AAABBZBD") == -1
