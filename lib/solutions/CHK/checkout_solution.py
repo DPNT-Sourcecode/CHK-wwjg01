688ee5ead245829b170990b9072aa95793035b86
sbx_user1051 <sbx_user1051@169.254.123.133>
2024-07-21 15:45:29 +0000

    Sun Jul 21 15:45:29 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,28 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+# skus - a string containing items in basket
+'''
+Our price table and offers: 
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+'''
+
+# Implement the price table as a dict
+# key - item SKU / id, value - list where 0th index is price, following indexes are tuples representing bulk deal, if any
+price_table = {
+    "A": [50, (3, 130)],
+    "B": [30, (2, 45)],
+    "C": [20],
+    "D": [15]
+}
 def checkout(skus):
     raise NotImplementedError()
+
